fix off by one while loop error in chunking

audio_ops.py
@@ -6,6 +6,7 @@ import scipy.io.wavfile
 import torch
 import torchaudio
 
+
 from constants import LOCAL_CHUNK_FILEPATHS, \
     LOCAL_WAV_FILEPATHS, WAV_CHUNK_SIZE
 
@@ -33,7 +34,7 @@ def chunk_audio(wav_filepath, chunk_size=WAV_CHUNK_SIZE):
     length = np_audio.shape[0]
     i = 0
     chunks = []
-    while i + chunk_size < length:
+    while i + chunk_size <= length:
         np_chunk = np_audio[i: i + chunk_size]
         chunks.append(np_chunk)
         i += chunk_size
@@ -57,7 +58,9 @@ def chunks_dir_to_numpy(chunks_dir):
 
 
 def chunks_to_numpy(chunks):
-    return np.vstack([mu_transform(v) for v in chunks])
+    mu_chunks = list(mu_transform(v) for v in chunks)
+    z = np.array(mu_chunks)
+    return z
 
 
 def mu_transform(v):

training.py
@@ -12,7 +12,6 @@ from constants import WAV_CHUNK_SIZE, \
 from audio_ops import chunks_dir_to_numpy
 
 
-
 def train_pytorch(batch_size, n_epochs):
 
     if not os.path.exists(MODEL_SAVE_DIR):
@@ -41,7 +40,7 @@ def train_pytorch(batch_size, n_epochs):
             if i + batch_size >= N:
                 k = i + batch_size - N
                 indices = torch.cat((permutation[i:], permutation[:k]), 0)
-                assert (len(indices) == batch_size)
+                #assert (len(indices) == batch_size)
             else:
                 indices = permutation[i:i+batch_size]
             batch_x = x_train[indices]